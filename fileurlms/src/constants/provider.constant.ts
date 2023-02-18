653e935f2d483b8c95c318de24b0aaec116d103b
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-02-18 17:08:00 +0600

    db provider configured

---
+++ fileurlms/src/constants/provider.constant.ts
@@ -0,0 +1,1 @@
+export const MYSQL_DATA_SOURCE = "MYSQL_DATA_SOURCE";
--- fileurlms/src/fileurl.module.ts
+++ fileurlms/src/fileurl.module.ts
@@ -1,10 +1,9 @@
 import { Module } from "@nestjs/common";
 import { FileUrlController } from "./fileurl.controller";
 import { fileUrlProvider } from "./fileurl.provider";
-import { DatabaseModule } from "src/modules/database/database.module";
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [],
   controllers: [FileUrlController],
   providers: [...fileUrlProvider]
 })
